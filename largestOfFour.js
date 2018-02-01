bdcfc867ade3ccbfc8019f88257a7102bdffc1ab
Jonathan Tuala <jetuala@gmail.com>
2018-01-31 22:24:33 -0800

    largestOfFour.js

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,13 +1,11 @@
-var newArr = [];
-function compareNumbers(a, b) {
-  return b - a;
-}
-
 function largestOfFour(arr) {
-  for (var i=0; i > 4; i++) {
-    newArr.push(arr[i].sort(compareNumbers));
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].sort(function(a,b){return b - a;})[0]);
   }
   return newArr;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]);
