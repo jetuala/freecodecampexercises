0c88d1835c9579cc1a58390cd2267bb25a53f101
Jonathan Tuala <jetuala@gmail.com>
2018-02-02 21:37:12 -0800

    slasher

--- slasher.js
+++ slasher.js
@@ -1,12 +1,7 @@
 function slasher(arr, howMany) {
   // it doesn't always pay to be first
-  var newArr = [];
-  if (howMany === 0) {
-    return arr;
-  } else {
-    newArr = arr.splice(howMany, arr.length - 1);
-    return newArr;
-  }
+  arr.splice(0, howMany);
+  return arr;
 }
 
 slasher([1, 2, 3], 2);
